62482cb9436af54aa642eb69432a5b50ae232144
Atsushi Kokubu <akokubu@gmail.com>
2022-02-21 22:56:09 +0900

    ecs

--- envs/prod/app/foobar/data.tf
+++ envs/prod/app/foobar/data.tf
@@ -1,3 +1,23 @@
 data "aws_caller_identity" "self" {}
 
 data "aws_region" "current" {}
+
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+
+  config = {
+    bucket = "lp-laravel-fargate-app-tfstate"
+    key    = "${local.system_name}/${local.env_name}/network/main_v1.0.0.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "routing_longposition_ml" {
+  backend = "s3"
+
+  config = {
+    bucket = "lp-laravel-fargate-app-tfstate"
+    key    = "${local.system_name}/${local.env_name}/routing/longposition_ml_v1.0.0.tfstate"
+    region = "ap-northeast-1"
+  }
+}
